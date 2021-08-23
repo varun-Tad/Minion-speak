046c987b33844da3d876b03dbd92da20a878a903
varun-Tad <varuntad141@gmail.com>
2021-08-24 01:29:55 +0530

    added mocks server

--- app.js
+++ app.js
@@ -2,11 +2,34 @@ var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text)
+{
+   return serverURL + "?" + "text=" + text;
+}
 
 
+function errorHandler()
+{
+   console.log("error occured", error);
+   alert('something wrong with server! Try again later.');
+}
 
 function clickHandler(){
-   outputDiv.innerText = " ofwofowjofwjofconow";
+   
+
+   var inputText = txtInput.value;  //taking input here
+
+   fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json=>{
+      outputDiv.innerText= json.contents.translated
+    })
+     
+    .catch(errorHandler);    // Handles errors in case the server crashes
 
 }
 btnTranslate.addEventListener('click',clickHandler)
